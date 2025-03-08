4d2be0352d7bc13571b96401d38da00871663dd1
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-03-08 20:44:06 +0000

    updated project name on package.json and initialised dateUtils.js with funcion names to be built

---
+++ src/dateUtils.js
@@ -0,0 +1,33 @@
+/**
+ * Formats a date object into a string according to a specified format
+ * @param {Date} date - The date object to format. 
+ * @param {string} format - The format string (eg. "DD/MM/YYYY" or "YYYY-MM-DD").
+ * @returns {string | undefined} The formatted date string or undefined if the date is invalid.
+ */
+export function formatDate(date, format) {
+
+}
+
+export function parseDate(dateString, format) {
+
+}
+
+export function getDayOfTheWeek(date) {
+
+}
+
+export function diffInDays(date1, date2) {
+
+}
+
+export function diffInMonths(date1, date2) {
+
+}
+
+export function diffInYears(date1, date2) {
+
+}
+
+export function isLeapYear(year) {
+
+}
