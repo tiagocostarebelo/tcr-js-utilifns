c8031354256d524c0ad9e53da1b5ec436bdf3c1f
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-03-08 01:01:19 +0000

    Initialize numberUtils.js functions with JSDoc for documentation purposes and value validation for robustness

--- src/numberUtils.js
+++ src/numberUtils.js
@@ -1,13 +1,71 @@
+/**
+ * Checks if a number is even
+ * @param {number} number - The number to check.
+ * @returns {boolean | undefined} True if the number is even, false otherwise. Returns undefined if the input is not a valid number.
+ */
 export function isEven(number) {
+    if (typeof number !== 'number' || isNaN(number)) {
+        console.error("isEven: Input must be a valid number.");
+        return undefined;
+    }
+
     return number % 2 === 0;
 }
 
+/**
+ * Checks if a number is odd
+ * @param {number} number - The number to check.
+ * @returns {boolean | undefined} True if the number is odd, false otherwise. Returns undefined if the input is not a valid number.
+ */
 export function isOdd(number) {
+    if (typeof number !== 'number' || isNaN(number)) {
+        console.error("isOdd: Input must be a valid number.");
+        return undefined;
+    }
+
     return number % 2 !== 0;
 }
 
-export function roundTo(decimalPlaces, number) {
-    return Number(number.toFixed(decimalPlaces));
+/**
+ * Rounds a number to a specified number of decimal places
+ * @param {number} number - The number to round.
+ * @param {number} decimalPlaces - The number of decimal places to round to. Defaults to 0.
+ * @returns {number | NaN} The rounded number.Returns NaN if the inputs are not valid numbers.
+ */
+export function roundTo(number, decimalPlaces) {
+    if (typeof number !== 'number' || isNaN(number)) {
+        console.error("roundTo: Input number must be valid number.");
+        return NaN;
+    }
+
+    if (typeof decimalPlaces !== 'number' || isNan(decimalPlaces)) {
+        console.error("roundTo: Input decimalPlaces must be a valid number. Using default value of 0.");
+        decimalPlaces = 0;
+    }
+
+    const multiplier = Math.pow(10, decimalPlaces);
+    return Math.round(number * multiplier) / multiplier;
 }
 
+/**
+ * Generates a random integer within a specified range (inclusive)
+ * @param {number} min - The minimum value of the range.
+ * @param {number} max - The maximum value of the range.
+ * @returns {number | NaN} A random integer between min and max (inclusive), or NaN if min or max are not valid numbers.
+ */
+export function getRandomInt(min, max) {
+    if (typeof min !== 'number' || typeof max !== 'number' || isNaN(min) || isNaN(max)) {
+        console.error("getRandomInt: min and max must be valid numbers");
+        return NaN;
+    }
 
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    if (min > max) {
+        console.warn("getRandomInt: min cannot be greated than max. Swapping values.");
+        [min, max] = [max, min];
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
