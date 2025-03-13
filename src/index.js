329d867d07a15dc3bbf80a7d10b8f6c2caa4d6b4
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-03-13 23:33:59 +0000

    updated index file with exports

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-export * as numberUtils from './numberUtils.js';
 export * as dateUtils from './dateUtils.js';
+export * as numberUtils from './numberUtils.js';
+export * as objectUtils from './objectUtils.js';
+export * as stringUtils from './stringUtils.js';
