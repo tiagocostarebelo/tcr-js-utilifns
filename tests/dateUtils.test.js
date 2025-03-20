aa2588c84931e1e8b1917c8b9fc59612059cc3fe
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-03-20 23:07:15 +0000

    created first dateUtils test. Required to install babel and set config files for bable and jest to handle the new ES6 for Jest testing. deleted previously created test files, to focus solely one by one to avoid any undesired errors while testing runs

--- tests/dateUtils.test.js
+++ tests/dateUtils.test.js
@@ -6,3 +6,13 @@ import {
     isLeapYear
 } from '../src/dateUtils';
 
+describe('Date Utilities', () => {
+
+    describe('getDayOfTheWeek', () => {
+        test('should return correct day of the week for a valid date', () => {
+            const date = new Date("2025-03-07");
+            expect(getDayOfTheWeek(date)).toBe("Friday");
+        })
+    })
+
+})
