01c2722756e363d20ad6d0845bcebc448ed3e7df
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-03-21 16:50:33 +0000

    updated numberUtils function tests

--- tests/numberUtils.test.js
+++ tests/numberUtils.test.js
@@ -74,8 +74,58 @@ describe('Number Utilities', () => {
             expect(console.error).toHaveBeenCalledWith("roundTo: decimalPlaces must be a non-negative number.");
             console.error.mockRestore();
         });
+    });
 
+    // getRandomInt function test
+    describe('getRandomInt', () => {
+        it('Should generate a random integer within the specified range', () => {
+            const min = 1;
+            const max = 10;
+            const numberOfTests = 100;
+
+            for (let i = 0; i < numberOfTests; i++) {
+                const randomNumber = getRandomInt(min, max);
+                expect(randomNumber).toBeGreaterThanOrEqual(min);
+                expect(randomNumber).toBeLessThanOrEqual(max);
+                expect(Number.isInteger(randomNumber)).toBe(true);
+            }
+        });
+
+        it('should return NaN if min or max are invalid numbers', () => {
+            console.error = jest.fn();
+            expect(getRandomInt("not a number", 10)).toBeNaN();
+            expect(getRandomInt(1, "not a number")).toBeNaN();
+            expect(getRandomInt(NaN, 10)).toBeNaN();
+            expect(getRandomInt(1, NaN)).toBeNaN();
+            expect(console.error).toHaveBeenCalled();
+            console.error.mockRestore();
+        });
+
+        it('Should generate a random integer within the swapped range if min > max', () => {
+            console.warn = jest.fn();
+            const min = 10;
+            const max = 1;
+            const numberOfTests = 100;
+
+            for (let i = 0; i < numberOfTests; i++) {
+                const randomNumber = getRandomInt(min, max);
+                expect(randomNumber).toBeGreaterThanOrEqual(max);
+                expect(randomNumber).toBeLessThanOrEqual(min);
+                expect(Number.isInteger(randomNumber)).toBe(true);
+            }
+            expect(console.warn).toHaveBeenCalledWith("getRandomInt: min cannot be greater than max. Swapping values.");
+            console.warn.mockRestore();
+        });
+    });
+
+    // sum function test 
+    describe('sum', () => {
+        it('should return the sum of all numbers in an array', () => {
+            const numbersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            expect(sum(numbersArr)).toBe(55);
+        })
     })
+
 
 
 
